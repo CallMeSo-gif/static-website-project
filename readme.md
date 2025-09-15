f801d8ade1779de4e7c00b9fe6951853051da6fc
CallMeSo-gif <sorelle.feuvou.kana@devoteam.com>
2025-09-15 11:20:38 +0200

    remove the state and the provider/modules files from git status

---
+++ readme.md
@@ -0,0 +1,21 @@
+Title : Project Brief: Sprint for IaC & CI/CD Fundamentals
+
+
+This readme is about our first project. 
+
+1. Objective
+
+This project is a 3-day intensive sprint designed to provide a foundational, hands-on understanding of modern DevOps practices. The primary goal is to build a fully automated CI/CD pipeline for deploying a static website to AWS S3. The project leverages Terraform for Infrastructure as Code (IaC) and GitHub Actions for automation, demonstrating a complete, version-controlled workflow from code commit to live deployment.
+
+2. Key Technologies
+
+Infrastructure as Code (IaC): Terraform
+
+Cloud Provider: AWS (specifically S3 and IAM)
+
+CI/CD: GitHub Actions
+
+Version Control: Git & GitHub
+
+Content: HTML
+
---
+++ terraform/main.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+
+resource "aws_s3_bucket" "tf_bucket" {
+  bucket = var.bucket_name
+
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.tf_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+/*
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+  }
+
+  */
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.tf_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.tf_bucket.id
+  policy = data.aws_iam_policy_document.public_read_policy.json
+  depends_on = [ aws_s3_bucket_public_access_block.public_access ]
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.tf_bucket.id
+  key    = "index.html"
+  source = "../index.html"
+ # etag   = filemd5("../website/index.html")
+  content_type = "text/html"
+}
+
+
+# =======================================================
+# IAM Policy to allow public read access to the bucket
+# =======================================================
+# This policy is to allow public read access to the S3 bucket
+data "aws_iam_policy_document" "public_read_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.tf_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    effect = "Allow"
+  }
+
+}
+
+output "name" {
+  value = aws_s3_bucket.tf_bucket.bucket
+}
